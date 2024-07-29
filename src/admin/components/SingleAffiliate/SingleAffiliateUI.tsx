5803444b91b566eb0b8ab780e2e631e40b27e769
Avinash Patel <avinashpatel9755@gmail.com>
2024-07-29 08:16:33 +0530

    add Back Button Function

--- src/admin/components/SingleAffiliate/SingleAffiliateUI.tsx
+++ src/admin/components/SingleAffiliate/SingleAffiliateUI.tsx
@@ -1,12 +1,21 @@
 import { ArrowLeftMini } from "@medusajs/icons";
 import { CustomerDetailCard } from "./CustomerDetailCard";
 import { AffiliateDetailsCard } from "./AffiliateDetailsCard";
+import { useNavigate } from "react-router-dom";
 
 const SingleAffiliateUI = () => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1); // This navigates back to the previous page in the history stack
+  };
   return (
     <div className="w-full flex flex-col gap-8 p-6">
       <div>
-        <button className="flex items-center justify-start gap-2 cursor-pointer">
+        <button
+          onClick={handleBack}
+          className="flex items-center justify-start gap-2 cursor-pointer"
+        >
           <div className="bg-[#eeeeee] p-1 rounded-full">
             <ArrowLeftMini />
           </div>
